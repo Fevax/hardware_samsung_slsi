4db638898a5e88d2d8dbbd3df0924734617ac281
fevax <fer.vonarx@gmail.com>
2015-07-12 15:10:21 -0300

    mk of csc modified to mach with CM & added csc_helper

--- exynos/libcsc/Android.mk
+++ exynos/libcsc/Android.mk
@@ -2,15 +2,54 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := \
+	csc_helper.c
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+ifneq ($(BOARD_USE_KHRONOS_OMX_HEADER),true)
+LOCAL_C_INCLUDES += \
+	frameworks/native/include/media/openmax
+endif
+
+LOCAL_C_INCLUDES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+	system/core/include \
+	hardware/samsung_slsi/openmax/include/khronos \
+	hardware/samsung_slsi/openmax/include/exynos \
+	hardware/samsung_slsi/exynos5-insignal/include \
+	$(LOCAL_PATH)/../include
+
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_CFLAGS := \
+	-DUSE_SAMSUNG_COLORFORMAT \
+	-DEXYNOS_OMX
+
+LOCAL_MODULE := libcsc_helper
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := liblog
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
 	csc.c
 
 LOCAL_C_INCLUDES := \
-	hardware/samsung_slsi/$(TARGET_BOARD_PLATFORM)/include \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+	hardware/samsung_slsi/exynos5-insignal/include \
+	hardware/samsung_slsi/openmax/include/khronos \
+	hardware/samsung_slsi/openmax/include/exynos \
 	$(LOCAL_PATH)/../include \
 	$(LOCAL_PATH)/../libexynosutils
+
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 
@@ -23,6 +62,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 
 LOCAL_STATIC_LIBRARIES := libswconverter
+LOCAL_WHOLE_STATIC_LIBRARIES := libcsc_helper
 LOCAL_SHARED_LIBRARIES := liblog libexynosutils
 
 LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
@@ -42,5 +82,7 @@ endif
 
 LOCAL_CFLAGS += -DUSE_ION
 LOCAL_SHARED_LIBRARIES += libion_exynos
+
+LOCAL_CFLAGS += -DEXYNOS_OMX
 
 include $(BUILD_SHARED_LIBRARY)
